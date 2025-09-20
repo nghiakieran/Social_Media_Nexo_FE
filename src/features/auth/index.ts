fc13067cf9e755936e9d68c3991ad2973b20dfdb
nghiakieran <lechinghia202@gmail.com>
2025-09-20 14:46:57 +0700

    Fix index file import

---
+++ src/features/auth/index.ts
@@ -0,0 +1,10 @@
+// Types
+export * from './types';
+
+// Components
+export { SwitchAccountDialog } from './components/SwitchAccountDialog';
+export { ForgotPasswordDialog } from './components/ForgotPasswordDialog';
+
+// Pages
+export { ResetPasswordPage } from './pages/ResetPasswordPage';
+export { VerifyResetCodePage } from './pages/VerifyResetCodePage';
