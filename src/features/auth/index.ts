e2461cf373774eeee2f1a7e531f5cbf3bbcdd9ee
nghiakieran <lechinghia202@gmail.com>
2025-09-20 14:50:46 +0700

    Fix import redudant

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -3,8 +3,3 @@ export * from './types';
 
 // Components
 export { SwitchAccountDialog } from './components/SwitchAccountDialog';
-export { ForgotPasswordDialog } from './components/ForgotPasswordDialog';
-
-// Pages
-export { ResetPasswordPage } from './pages/ResetPasswordPage';
-export { VerifyResetCodePage } from './pages/VerifyResetCodePage';
