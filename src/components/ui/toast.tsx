8e0cfd8b8793b1e976c30e2c38a0348ceab3b731
TPTNam <thanhnamtran2602@gmail.com>
2026-06-27 11:04:41 +0700

    fix: update show toast for mobile

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 gap-3 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 gap-3 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px] max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:w-[calc(100%-32px)] max-sm:max-w-xs max-sm:top-2",
       className
     )}
     {...props}
@@ -23,7 +23,7 @@ const ToastViewport = React.forwardRef<
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
 const toastVariants = cva(
-  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-xl border p-5 pr-10 shadow-strong data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-2 data-[state=open]:sm:slide-in-from-bottom-2 backdrop-blur-xl bg-white/75 dark:bg-neutral-900/75 border-white/20 dark:border-white/10 duration-300 ease-in-out text-foreground fill-none outline-none ring-offset-background transition-transform transform-gpu",
+  "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-xl border p-5 pr-10 max-sm:p-3 max-sm:pr-8 shadow-strong data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-2 data-[state=open]:sm:slide-in-from-bottom-2 backdrop-blur-xl bg-white/75 dark:bg-neutral-900/75 border-white/20 dark:border-white/10 duration-300 ease-in-out text-foreground fill-none outline-none ring-offset-background transition-transform transform-gpu",
   {
     variants: {
       variant: {
@@ -42,6 +42,7 @@ const toastVariants = cva(
     },
   }
 )
+
 
 const Toast = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Root>,
